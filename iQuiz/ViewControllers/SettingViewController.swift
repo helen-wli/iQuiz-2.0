6617af5f301258b7fa7ccf048025110b0ccd2d6a
helen-wli <helenwyue@gmail.com>
2022-06-22 15:23:19 -0700

    implemented settingVC

--- iQuiz/ViewControllers/SettingViewController.swift
+++ iQuiz/ViewControllers/SettingViewController.swift
@@ -9,6 +9,79 @@ import UIKit
 
 class SettingViewController: UIViewController {
 
+    @IBOutlet weak var urlTextField: UITextField!
+    
+    // Fire an alert
+    func fireAlert(alertTitle : String, alertMessage : String) {
+        let alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: {
+            NSLog("\(alertTitle) fired")
+        })
+    }
+    
+    // Check whether the input is a valid url
+    private func isValidInput(_ input: String?) -> Bool {
+        // case: empty input
+        if input == "" {
+            fireAlert(alertTitle: "Empty Input", alertMessage: "Please enter a valid URL")
+            return false
+        }
+        
+        // for non-empty input, check whether the input is a valid URL
+        if let urlString = input {
+            if let url = NSURL(string: urlString) {
+                return UIApplication.shared.canOpenURL(url as URL)
+            }
+        }
+        return false
+    }
+    
+    // Fetch data
+    private func fetchQuizData(_ fetchingUrlStr : String) {
+        let request = URLRequest(url: URL(string: fetchingUrlStr)!)
+        
+        URLSession.shared.dataTask(with: request) { [weak self]  data, response, error in
+            
+            guard self != nil else { return }
+            
+            guard error == nil else {
+                print("Cannot parse data")
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200
+            else {
+                print("Error with http response")
+                return
+            }
+            
+            guard let data = data else {
+                print("No data found")
+                return
+            }
+            
+            if let quizData = try? JSONDecoder().decode([Quiz].self, from: data) {
+                DispatchQueue.main.async {
+                    // save data to the QuizData singleton
+                    QuizData.instance.quizzes = quizData
+                }
+            } else {
+                print("Cannot fetch data")
+                return
+            }
+        }.resume()
+    }
+    
+    @IBAction func checkButtonPressed(_ sender: UIButton) {
+        let textInput = urlTextField.text
+        if isValidInput(textInput) {
+            self.fetchQuizData(textInput!)
+        } else {
+            fireAlert(alertTitle: "Not a URL", alertMessage: "Please enter a valid URL")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
